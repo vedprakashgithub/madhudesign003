8cceb661cdf5e60dabb7ae5162cc42cec8c85ced
Ved Prakash <129796900+vedprakashgithub@users.noreply.github.com>
2025-08-14 22:03:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
     'https://madhudesigns.com',
     'https://admin.madhudesigns.com',
     'http://localhost:5173',
-    'http://localhost:5173'
+    'http://localhost:5174'
   ],
   credentials: true
 }));
