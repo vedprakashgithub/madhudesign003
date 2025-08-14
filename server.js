30c3e7004e1bacff5581929f28ed946e97f4b9f8
Ved Prakash <129796900+vedprakashgithub@users.noreply.github.com>
2025-08-14 21:27:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,12 @@ const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
+
+
 // middlewares
-app.use(cors({ 
-  origin: ["https://madhudesigns.com", "https://admin.madhudesigns.com", "http://localhost:5173", "https://admin.madhudesigns.com/list"] // Updated to use madhudesigns.com and admin.madhudesigns.com
+app.use(cors({
+  origin: ["https://madhudesigns.com", "https://admin.madhudesigns.com", "http://localhost:5173"],
+  credentials: true
 }));
 app.use(express.json());
 
